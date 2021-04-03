0fec91b94b0aa5e8100b08258f055b584acfa4f4
saurabh parasar <saurabhparasar3010@gmail.com>
2021-04-03 09:48:09 +0530

    coding challenge 3

---
+++ app.js
@@ -0,0 +1,52 @@
+     // data 1
+// let avgScoreDolphins = (96 + 108 + 89) / 3;
+// let avgScorekoalas = (88 + 110 + 91) / 3;
+
+// console.log(avgScoreDolphins, avgScorekoalas);
+
+// if(avgScoreDolphins > avgScorekoalas) {
+//     console.log(" team Dolphins are the winner");
+// }else if(avgScoreDolphins < avgScorekoalas){
+//     console.log(" team Koalas are the winner");
+// }else if(avgScoreDolphins === avgScorekoalas ){
+//     console.log(" both teams are the winner");
+// }
+
+    // bonus data1:
+
+
+// let avgScoreDolphins = (97 + 112 + 101) / 3;
+// let avgScorekoalas = (109 + 95 + 123) / 3;
+
+// console.log(avgScoreDolphins, avgScorekoalas);
+
+
+// if(avgScoreDolphins > avgScorekoalas && avgScoreDolphins >= 100) {
+//     console.log(" team Dolphins are the winner");
+// }else if(avgScoreDolphins < avgScorekoalas && avgScorekoalas >= 100 ){
+//     console.log(" team Koalas are the winner");
+// }else if(avgScoreDolphins === avgScorekoalas ){
+//     console.log(" both teams are the winner");
+// }
+
+//   bonus data2
+
+// let avgScoreDolphins = (97 + 112 + 101) / 3;
+// let avgScorekoalas = (109 + 95 + 106) / 3;
+
+// console.log(avgScoreDolphins, avgScorekoalas);
+
+
+// if(avgScoreDolphins > avgScorekoalas && avgScoreDolphins >= 100) {
+//     console.log(" team Dolphins are the winner");
+// }else if(avgScoreDolphins < avgScorekoalas && avgScorekoalas >= 100 ){
+//     console.log(" team Koalas are the winner");
+// }else if(avgScoreDolphins === avgScorekoalas && avgScoreDolphins >= 100 && avgScorekoalas >= 100){
+//     console.log(" both teams are the winner");
+// }else {
+//     console.log("no one wins");
+// }
+
+// assignment
+
+
